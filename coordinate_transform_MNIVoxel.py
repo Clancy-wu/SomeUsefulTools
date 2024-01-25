b21b1280857b26f14162b1913d279ed38d37bd6d
Kang Wu <53364840+Clancy-wu@users.noreply.github.com>
2024-01-25 16:37:48 -0600

    Update coordinate_transform_MNIVoxel.py

--- coordinate_transform_MNIVoxel.py
+++ coordinate_transform_MNIVoxel.py
@@ -9,13 +9,40 @@ def mmToVox(mmcoords):
 	return voxcoords
 	
 ## a better one that is corresponded to FSLeyes.
-def mmToVox_fsleyes(mmcoords):
+def mmToVox_fsleyes_2mm(mmcoords):
 	#function to convert mm coordinates in the standard 2mm MNI atlas into voxel coordinates
 	voxcoords = ['','','']
 	voxcoords[0] = str(int(round(mmcoords[0]/2))*-1+45)
 	voxcoords[1] = str(int(round(mmcoords[1]/2))+63)
 	voxcoords[2] = str(int(round(mmcoords[2]/2))+36)
 	return voxcoords
+
+def mmToVox_fsleyes_0_5mm(mmcoords):
+	#function to convert mm coordinates in the standard 0.5mm MNI atlas into voxel coordinates
+      # FSL space
+	voxcoords = ['','','']
+	voxcoords[0] = str(int(round((mmcoords[0])/0.5))*-1+180)
+	voxcoords[1] = str(int(round((mmcoords[1])/0.5))+252)
+	voxcoords[2] = str(int(round((mmcoords[2])/0.5))+144)
+	return voxcoords  
+
+def mmToVox_fsleyes_1mm(mmcoords):
+	#function to convert mm coordinates in the standard 1mm MNI atlas into voxel coordinates
+      # FSL space
+	voxcoords = ['','','']
+	voxcoords[0] = str(int(round((mmcoords[0])/1))*-1+90)
+	voxcoords[1] = str(int(round((mmcoords[1])/1))+126)
+	voxcoords[2] = str(int(round((mmcoords[2])/1))+72)
+	return voxcoords  
+
+def mmToVox_fsleyes_xmm(mmcoords, x):
+	#function to convert mm coordinates in the standard 1mm MNI atlas into voxel coordinates
+      # FSL space
+	voxcoords = ['','','']
+	voxcoords[0] = str(int(round((mmcoords[0])/x))*-1+(90/x))
+	voxcoords[1] = str(int(round((mmcoords[1])/x))+(126/x))
+	voxcoords[2] = str(int(round((mmcoords[2])/x))+(72/x))
+	return voxcoords 
 	
 #### 2. Voxel coordinates to MNI coordinates
 def coords(inputimage, i, j, k):
