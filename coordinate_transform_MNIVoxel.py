bf76a6343ccd89fa32e3c8b1cc3e34710cae04f5
Kang Wu <53364840+Clancy-wu@users.noreply.github.com>
2024-01-25 16:01:58 -0600

    Update coordinate_transform_MNIVoxel.py

--- coordinate_transform_MNIVoxel.py
+++ coordinate_transform_MNIVoxel.py
@@ -7,7 +7,16 @@ def mmToVox(mmcoords):
 	voxcoords[1] = str(int(round(int(mmcoords[1])/2))+63)
 	voxcoords[2] = str(int(round(int(mmcoords[2])/2))+36)
 	return voxcoords
-  
+	
+## a better one that is corresponded to FSLeyes.
+def mmToVox_fsleyes(mmcoords):
+	#function to convert mm coordinates in the standard 2mm MNI atlas into voxel coordinates
+	voxcoords = ['','','']
+	voxcoords[0] = str(int(round(mmcoords[0]/2))*-1+45)
+	voxcoords[1] = str(int(round(mmcoords[1]/2))+63)
+	voxcoords[2] = str(int(round(mmcoords[2]/2))+36)
+	return voxcoords
+	
 #### 2. Voxel coordinates to MNI coordinates
 def coords(inputimage, i, j, k):
     """Returns X, Y, Z coordinates for i, j, k
@@ -19,27 +28,8 @@ def coords(inputimage, i, j, k):
     abc = inputimage.affine[:3, 3]
     return M.dot([i, j, k]) + abc
 
-
-nilearn.image.coord_transform(x, y, z, affine)
 # the function "coord_transform" in the nilearn can transform
 # voxel space to the image space you choose (either MNI or T1w)
+nilearn.image.coord_transform(x, y, z, affine)
 
-import numpy as np
-import nibabel as nib
 
-# Load your transformation matrix
-transform_matrix = np.array([[a, b, c, d],
-                              [e, f, g, h],
-                              [i, j, k, l],
-                              [0, 0, 0, 1]])  # Example transformation matrix
-# Load voxel size
-voxel_size = np.array([voxel_size_x, voxel_size_y, voxel_size_z])  # Example voxel size
-# Load MNI coordinates
-mni_coordinates = np.array([x_mni, y_mni, z_mni])  # Example MNI coordinates
-# Inverse transformation
-inverse_transform = np.linalg.inv(transform_matrix)
-voxel_coordinates = np.dot(np.hstack((mni_coordinates, 1)), inverse_transform)[:3]
-# Convert to voxel coordinates
-voxel_coordinates /= voxel_size
-print("Voxel coordinates:", voxel_coordinates)
-  
